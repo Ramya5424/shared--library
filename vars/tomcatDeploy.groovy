a6917a6cb8feb80426b3bc0399da3b85451703b3
Ramya5424 <ramyaelumalai114@gmail.com>
2025-07-23 22:18:12 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,5 +1,8 @@
 def call(tomcatIp, warfileName, tomcatUser, remoteUser, credId) {
-    sshagent([tomcatdev]) {
+    sshagent(['credId']) {
+        
+    }
+    
         sh "scp target/${warfileName} ${tomcatUser}@${tomcatIp}:/opt/tomcat10/webapps"
         sh "ssh ${tomcatUser}@${tomcatIp} /opt/tomcat10/bin/shutdown.sh"
         sh "ssh ${tomcatUser}@${tomcatIp} /opt/tomcat10/bin/startup.sh"
